dcd0532ef67c0e92d0295d63d35567f99647a73b
노윤진 <youndrive1@gmail.com>
2020-07-15 21:05:01 +0900

    dd

--- blogprj/settings.py
+++ blogprj/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WHiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blogprj.urls'
